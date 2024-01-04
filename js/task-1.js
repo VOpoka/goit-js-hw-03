09b839aaae89976df669986ca1aff766d3e0ec33
VOpoka <144518288+VOpoka@users.noreply.github.com>
2024-01-04 17:36:35 +0100

    hw-03/1

--- js/task-1.js
+++ js/task-1.js
@@ -4,7 +4,7 @@ function slugify(title) {
   const slugMassive = slug.split(" ");
   return slugMassive.join("-");
 }
-console.log(slugify("Arrays for begginers"));
-console.log(slugify("English for developer"));
-console.log(slugify("Ten secrets of JavaScript"));
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+// console.log(slugify("Arrays for begginers"));
+// console.log(slugify("English for developer"));
+// console.log(slugify("Ten secrets of JavaScript"));
+// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
