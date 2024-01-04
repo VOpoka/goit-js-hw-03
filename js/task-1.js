f4bd9dd8c512922a0d9f1ebb4c7438be14899076
VOpoka <144518288+VOpoka@users.noreply.github.com>
2024-01-04 17:34:42 +0100

    hw-03

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+"use strict";
+function slugify(title) {
+  const slug = title.toLowerCase();
+  const slugMassive = slug.split(" ");
+  return slugMassive.join("-");
+}
+console.log(slugify("Arrays for begginers"));
+console.log(slugify("English for developer"));
+console.log(slugify("Ten secrets of JavaScript"));
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
